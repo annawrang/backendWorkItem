23b4d440acbe912ba7c30db8aa76137ed6111089
Anna Wrang <anna.wrang@gmail.com>
2018-05-07 23:51:08 +0200

    litet test anna

--- src/main/java/se/dajo/taskBackend/resource/UserResource.java
+++ src/main/java/se/dajo/taskBackend/resource/UserResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-// HEJ
+// hej
 @Component
 @Path("users")
 @Produces(MediaType.APPLICATION_JSON)
