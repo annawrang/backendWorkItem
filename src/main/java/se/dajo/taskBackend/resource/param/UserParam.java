720f50baf1a03c9530ebcc8deb75160285ab42fd
Anna Wrang <anna.wrang@gmail.com>
2018-05-16 14:31:17 +0200

    fixat till monstermetoden

--- src/main/java/se/dajo/taskBackend/resource/param/UserParam.java
+++ src/main/java/se/dajo/taskBackend/resource/param/UserParam.java
@@ -1,31 +1,15 @@
 package se.dajo.taskBackend.resource.param;
 
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.QueryParam;
 
 public final class UserParam {
 
     @QueryParam("firstName")
-    @DefaultValue("0")
-    private String firstName;
+    public String firstName;
 
     @QueryParam("surName")
-    @DefaultValue("0")
-    private String surName;
+    public String surName;
 
     @QueryParam("userNumber")
-    @DefaultValue("0")
-    private Long userNumber;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getSurName() {
-        return surName;
-    }
-
-    public Long getUserNumber() {
-        return userNumber;
-    }
+    public Long userNumber;
 }
