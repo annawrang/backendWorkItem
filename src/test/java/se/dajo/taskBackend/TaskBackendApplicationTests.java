403a780ef5ecfa2ebba264af285540e946098405
David Larsson <david.larsson89@gmail.com>
2018-08-23 14:43:44 +0200

    Trying to test tasks

--- src/test/java/se/dajo/taskBackend/TaskBackendApplicationTests.java
+++ src/test/java/se/dajo/taskBackend/TaskBackendApplicationTests.java
@@ -1,17 +1,30 @@
 package se.dajo.taskBackend;
 
-/*
+
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import se.dajo.taskBackend.enums.TaskStatus;
+import se.dajo.taskBackend.model.data.Task;
+import se.dajo.taskBackend.repository.TaskRepository;
+import se.dajo.taskBackend.resource.TaskResource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TaskBackendApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    TaskResource taskResource;
+    String description = "Eat";
+    TaskStatus status = TaskStatus.UNSTARTED;
+    Long taskNumber = 123555L;
 
-} */
+	@Test
+	public void createTaskTest() {
+	    Task task = taskResource.createTask(new Task(description, status, taskNumber));
+
+	}
+}
